e27ef4885a9977f656d87babc1f412f39bbbd442
Adesh Chaturvedi <adeshchaturvedi51@gmail.com>
2024-10-17 14:12:22 +0530

    learn tailwind framwork (login page) and grid practice

---
+++ css/my-tailwind-project/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"], // Add paths to HTML and JS files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
